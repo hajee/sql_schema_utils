60aaa7f61039d4dcdc281313feb41266a05fab45
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2016-04-29 10:02:29 +0200

    Fixed lint errors

--- tests/reinstall_schema_definition.pp
+++ tests/reinstall_schema_definition.pp
@@ -2,7 +2,7 @@ ora_schema_definition{'app@test':
   ensure      => '2.4.0',
   source_path => '/vagrant',
   reinstall   => true,
-  parameters => {
+  parameters  => {
     app_data_tablespace  => 'USERS',
     app_index_tablespace => 'USERS',
   }
--- tests/update_schema_definition.pp
+++ tests/update_schema_definition.pp
@@ -1,7 +1,7 @@
 ora_schema_definition{'app@test':
   ensure      => '2.4.0',
   source_path => '/vagrant',
-  parameters => {
+  parameters  => {
     app_data_tablespace  => 'USERS',
     app_index_tablespace => 'USERS',
   }
